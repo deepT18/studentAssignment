84112ec74991def213f66caad4978f32e82afa45
DeeptenduKuri <deeptendukuri@gmail.com>
2024-04-07 19:40:51 +0530

    Commit

--- src/pages/components/ProfileInformationRow.tsx
+++ src/pages/components/ProfileInformationRow.tsx
@@ -1,39 +1,41 @@
 import Image from "next/image";
-import avatarimg from "../../../public/avatar.png";
-import styles from "../../styles/BodyContent.module.css";
-// import CandidateProfile from "../CandidateProfile";
-// import { constants } from "perf_hooks";
+import proImage from "../../../public/avatar.png";
+// import styles from "../../styles/BodyContent.module.css";
 
 // @ts-ignore
-const InformationRow = ({handleClick, id, name, email, score }) => {
-  
+const InformationRow = ({ handleClick, id, name, email, score }) => {
 
   const handleClickonEachRow = () => {
-
     handleClick(id); // Pass the id to handleRowClick
   };
 
   return (
-    <div  onClick={handleClickonEachRow} className={styles.rowComponent}>
-      <div style={{display:"flex", flexDirection:"row"}}>
+    <div onClick={handleClickonEachRow} style={{
+      cursor: "pointer",
+      marginBottom: "20px",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      fontWeight: "bold"
+    }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
         <Image
-          style={{ height: "32px", width: "30px", borderRadius: "8px" , marginRight:"10px"}}
-          src={avatarimg}
+          style={{ height: "32px", width: "30px", borderRadius: "8px", marginRight: "10px" }}
+          src={proImage}
           alt="img"
         />
 
-        <div style={{ display: "flex", flexDirection: "column" , justifyContent:"center"}}>
-          <p style={{fontSize:"13px"}}>{name}</p>
-          <p style={{fontSize:"11px"}}>{email}</p>
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+          <p style={{ fontSize: "13px" }}>{name}</p>
+          <p style={{ fontSize: "11px" }}>{email}</p>
         </div>
       </div>
 
-      <span style={{fontSize:"100px", color:score>50 ? "#00B200" : "orange"}}>{score}%</span>
-    
+      <span style={{ fontSize: "100px", color: score > 50 ? "#00B200" : "orange" }}>{score}%</span>
+
     </div>
   );
 };
+
 export default InformationRow;
-{
-  /* <a href="" title="user icons">User icons created by kmg design - Flaticon</a> */
-}
